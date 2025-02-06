32289f1dbde035e74e337292c44eaa28b8c2a5d2
deepaksoni47 <deepaksoni23022004@gmail.com>
2025-02-06 19:35:55 +0530

    new files

---
+++ spotify clone/javascript/javascript part 2.js
@@ -0,0 +1,15 @@
+let num = 20;
+if (num % 10 ==0){
+    console.log("good");
+}else {
+    console.log("bad");
+}
+let name = prompt("Enter Your Name: ");
+let age = prompt("Enter Your Age: ");
+alert(`${name} is ${age} years old`);
+
+let quarter = 1;
+switch(quarter){
+    case 1 :
+        console.log("Jnauary")
+}
